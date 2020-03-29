f66acf44ae7f32f7a7c9e94f196b17f12079b36a
havsgard <60538559+havsgard@users.noreply.github.com>
2020-03-29 23:35:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// question 1
+const cat = {
+    complain: function() {
+      console.log("Meow!");
+    }
+  };
+
+
+// question 2
+const h3 = document.querySelector("h3");
+console.dir(heading);
+
+
+// question 3
+const h3 = document.querySelector("h3")
+h3.style.padding = "2em";
+
+
+// question 4
+h3.classList.add("subheading");
+console.log(h3.className);
+
+// question 5
+const paragraphs = document.querySelectorAll("p");
+
+// question 6
+function cats(catArray) {
+    console.log(catArray);
+    for (let i = 0; i < catArray.length; i++) {
+      const cats = "<h5>" + catArray[i].name + "</h5>";
+      console.log(catArray[i]);
+    }
+  }
+
+// question 7
+const cats = [
+    {
+      name: "Blob",
+      age: 10
+    },
+    {
+      name: "Harold"
+    },
+    {
+      name: "Blurt",
+      age: 21
+    }
+  ];
+  console.log(cats);
+
+
+// question 8
+function cats(catArray) {
+    console.log(catArray);
+    for (let i = 0; i < catArray.length; i++) {
+      const cats = "<h5>" + catArray[i].name + "</h5>";
+      console.log(catArray[i]);
+    }
+  }
+
+// question 9
+
+
+
+
+// question 10
+
